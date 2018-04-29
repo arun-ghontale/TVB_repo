9b69f3bc4b133b64ae31b4cbf79ab0df695bc3e4
arun-ghontale <arun.g.ghontale@gmail.com>
2018-04-29 10:36:14 +0530

    dataset and code for addition of products

--- add_products_to_list.py
+++ add_products_to_list.py
@@ -9,6 +9,8 @@ product_names = amazon_data['product_name']
 product_descriptions = amazon_data['description']
 product_prices = amazon_data['price']
 
+
+
 headers = {'User-Agent': 'Mozilla/5.0'}
 
 def clean(text):
@@ -34,3 +36,19 @@ for product_name,product_description,product_price in zip(product_names,product_
 		session.post('http://127.0.0.1:5500/api/products',headers=headers,data=payload)
 	except Exception as e: 
 		print(e)
+
+flipkart_data = pd.read_csv('flipkart_com-ecommerce_sample.csv',encoding = 'utf-8')
+
+product_names = flipkart_data['product_name'] 
+product_descriptions = flipkart_data['description']
+product_prices = flipkart_data['retail_price']
+
+for product_name,product_description,product_price in zip(product_names,product_descriptions,product_prices):
+	try:
+		payload = {'optype':'add','name':clean(product_name),'price':product_price,'description':clean(product_description)}
+		print(payload)
+		session = requests.Session()
+		session.post('http://127.0.0.1:5500/api/products',headers=headers,data=payload)
+	except Exception as e: 
+		print(e)
+
